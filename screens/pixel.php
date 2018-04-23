05b08102db8fc18e046f6b185f685c4a0eb9067a
nimrod-cohen <nimrod.cohen@gmail.com>
2018-04-23 17:13:52 +0300

    pixel comment design issue

--- screens/pixel.php
+++ screens/pixel.php
@@ -6,7 +6,7 @@
 	<div class="form-group">
 		<label for="pixel" class="form-field">Conversion pixel</label>
 		<input type="text" class="form-control" id="pixel" name="pixel"  placeholder="Ex. https://www.mydomain.com/?utm_source={source}&utm_campaign={campaign}&event={event}" value="<?php echo $pixel; ?>">
-		<label class="description">Use {source}, {campaign} to receive traffic source info,<br>and {event} placeholder to get type of conversion, available events: 'register','authenticated','payment'</label>
+		<label class="description">Use {source}, {campaign} to receive traffic source info, and {event} placeholder to get type of conversion, available events: 'register','authenticated','payment'</label>
 	</div>
 	<div style="margin-top:20px;clear: both">
 		<input type="submit" class="form-button" value="Save">
