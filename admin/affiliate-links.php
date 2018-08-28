596d37635f44a2b4f03bcce43faf25d47e6a2fcf
nimrod-cohen <nimrod.cohen@gmail.com>
2018-08-28 18:11:10 +0300

    affiliate links table in admin

---
+++ admin/affiliate-links.php
@@ -0,0 +1,27 @@
+<table class="wp-list-table widefat striped posts">
+	<thead>
+	<tr>
+		<th>ID</th>
+		<th>Url</th>
+		<th>Creation Date</th>
+		<th>Clicks</th>
+		<th>Registrations</th>
+		<th>Conversions</th>
+	</tr>
+	</thead>
+	<tbody>
+	<?php
+	$page = isset($_POST["paged"]) ? $_POST["paged"] : 0;
+	$links = AFMStats::affLinkStats($aff->ID(),$page,20);
+	foreach($links as $link) { ?>
+		<tr>
+			<td><?php echo $link["id"]; ?></td>
+			<td class="left"><?php echo $link["url"]."?uid=".$link["aff_id"]."&sid=".$link["id"]; ?></td>
+			<td><?php echo $link["created"]; ?></td>
+			<td><?php echo $link["click"]; ?></td>
+			<td><?php echo $link["register"]; ?></td>
+			<td><?php echo $link["first_deposit"]; ?></td>
+		</tr>
+	<?php }	?>
+	</tbody>
+</table>
--- admin/edit-affiliate.php
+++ admin/edit-affiliate.php
@@ -30,10 +30,14 @@
 	<div class="aff_content_cell" id="aff_content_top">
 		<h2 class="nav-tab-wrapper" id="aff-tabs">
 			<a class="nav-tab <?php echo $activePage == 'accounting' ? 'nav-tab-active' : ''; ?>" id="accounting-tab" href="#top#accounting">Accounting</a>
+			<a class="nav-tab <?php echo $activePage == 'links' ? 'nav-tab-active' : ''; ?>" id="links-tab" href="#top#links">Links</a>
 			<a class="nav-tab <?php echo $activePage == 'settings' ? 'nav-tab-active' : ''; ?>" id="settings-tab" href="#top#settings">Settings</a>
 		</h2>
 		<div id="tab-accounting" class="tab-view <?php echo $activePage == 'accounting' ? 'active' : ''; ?>">
 			<?php include_once("affiliate-accounting.php"); ?>
+		</div>
+		<div id="tab-links" class="tab-view <?php echo $activePage == 'links' ? 'active' : ''; ?>">
+			<?php include_once("affiliate-links.php"); ?>
 		</div>
 		<div id="tab-settings" class="tab-view <?php echo $activePage == 'settings' ? 'active' : ''; ?>">
 			<?php include_once("affiliate-settings.php"); ?>
